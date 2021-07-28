28c27022d3ac371fa96666211da7a2a007432b71
adzazarif <88089161+adzazarif@users.noreply.github.com>
2021-07-28 18:19:55 +0700

    upload web portfolio
    upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Photographer', 'Editor', 'Video Maker'];
+let count = 0;
+let txtIndex = 0;
+let curentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if (count == txtElement.length) {
+		 count = 0;
+	}
+
+	curentTxt = txtElement[count];
+
+	words = curentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == curentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500)
+
+})();
